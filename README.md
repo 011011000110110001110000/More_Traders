a48660c87ddb102f63e5ff02f1c3d3dd67ccea49
011011000110110001110000 <106524046011011000110110001110000@users.noreply.github.com>
2022-10-30 15:35:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# More Traders
+Makes wandering traders spawn 10 times as often compared to vanilla
+
+# License
+MIT License
---
+++ src/main/java/more/traders/MoreTraders.java
@@ -0,0 +1,19 @@
+package more.traders;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreTraders implements ModInitializer {
+    public static final String MOD_NAME = "More Traders";
+    public static final String MOD_VERSION = "1.0";
+    public static final String MOD_ID = "more-traders";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Loaded {} v.{}", MOD_NAME, MOD_VERSION);
+    }
+}
